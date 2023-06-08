abe7c867cebf9ee92b20fce599a4911cd2839aeb
Myodong <gyehd1230@naver.com>
2023-06-08 17:55:37 +0900

    Update MainActivity.kt

--- list_ex/app/src/main/java/com/myodong/list_ex/MainActivity.kt
+++ list_ex/app/src/main/java/com/myodong/list_ex/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
     }
 }
