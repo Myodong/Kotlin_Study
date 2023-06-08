b54766f4eaa7c2b39e50189eab9c7b6736e06222
Myodong <gyehd1230@naver.com>
2023-06-08 19:21:22 +0900

    .

--- listview/app/src/main/java/com/myodong/listview/ListViewAdapter.kt
+++ listview/app/src/main/java/com/myodong/listview/ListViewAdapter.kt
@@ -39,14 +39,17 @@ class ListViewAdapter(val List : MutableList<ListViewModel>) : BaseAdapter(){
 
         // A,B,C 값을 listview_item 넣어줘야함
         val title = convertView!!.findViewById<TextView>(R.id.listviewItem)
+        //  content 부분 가져오기
+        val content = convertView!!.findViewById<TextView>(R.id.listviewItem2)
+
         //List[position] List의 0 인덱스의 item 1인덱스 item 2인덱스 item을 하나하나 씩 넣어 들어가게 해라
         //title.text = List[position]
 
         // List[position].title title은 ListViewModel의 var title : String ="" 이다
         // a c e 가져오기
-//        title.text = List[position].title
+        title.text = List[position].title
         //b, d, f 가져오기
-        title.text = List[position].content
+        content.text = List[position].content
         // List[0] -> ListViewModel("a","b")
         // List[0] -> ListViewModel("c","d")
         // List[0] -> ListViewModel("e","f")
