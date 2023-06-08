9a2e24270753be96220a4a63f5d92d16ac13f5f1
Myodong <gyehd1230@naver.com>
2023-06-08 18:27:15 +0900

    Update ListViewAdapter.kt

--- listview/app/src/main/java/com/myodong/listview/ListViewAdapter.kt
+++ listview/app/src/main/java/com/myodong/listview/ListViewAdapter.kt
@@ -5,16 +5,22 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 
 class ListViewAdapter(val List : MutableList<String>) : BaseAdapter(){
+
+    // ListView의 item들의 갯수
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        // item들의 갯수는 List의 사이즈 만큼만 하겠다
+        return List.size
     }
 
+    // 아직 크게 신경 안써도 된다
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        // List의 item을 가져다 주고
+        return List[position]
     }
-
+    // 아직 크게 신경 안써도 된다
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
