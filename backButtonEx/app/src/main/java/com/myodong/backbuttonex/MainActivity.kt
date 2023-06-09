9ad8e2db8ddf1b178a371d2d1869c7d311f1decc
Myodong <gyehd1230@naver.com>
2023-06-09 09:32:46 +0900

    Update MainActivity.kt

--- backButtonEx/app/src/main/java/com/myodong/backbuttonex/MainActivity.kt
+++ backButtonEx/app/src/main/java/com/myodong/backbuttonex/MainActivity.kt
@@ -3,8 +3,11 @@ package com.myodong.backbuttonex
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    // 변수 선언
+    private  var isDouble = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -14,6 +17,9 @@ class MainActivity : AppCompatActivity() {
     override  fun onBackPressed(){
         // Log로 동작 하는지 확인해 보기
         Log.d("MainActivity","backbutton 작동")
+
+        isDouble = true
+        Toast.makeText(this, "한번더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show()
     }
 
 }
