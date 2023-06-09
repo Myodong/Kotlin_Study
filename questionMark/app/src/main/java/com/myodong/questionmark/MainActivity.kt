36f2a1d836435d674f5ca97c067ada89425de3e7
Myodong <gyehd1230@naver.com>
2023-06-09 09:19:36 +0900

    Update MainActivity.kt

--- questionMark/app/src/main/java/com/myodong/questionmark/MainActivity.kt
+++ questionMark/app/src/main/java/com/myodong/questionmark/MainActivity.kt
@@ -2,6 +2,7 @@ package com.myodong.questionmark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,11 @@ class MainActivity : AppCompatActivity() {
 
         val value : String? = "a"
         val value2 : String? = null
+
+        // ? -> null일 수 있음
+        // ! -> null이 아니다
         
+        //Log.d("MainActivity",value2!!) // 오류 발생 NullPointerException
+
     }
 }
