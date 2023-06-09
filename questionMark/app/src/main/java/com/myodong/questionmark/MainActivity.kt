e975db6765320106a9811d8112b4ecbf6f7e1e8a
Myodong <gyehd1230@naver.com>
2023-06-09 09:17:13 +0900

    Update MainActivity.kt

--- questionMark/app/src/main/java/com/myodong/questionmark/MainActivity.kt
+++ questionMark/app/src/main/java/com/myodong/questionmark/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+       super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val value : String? = "a"
+        val value2 : String? = null
+        
     }
 }
