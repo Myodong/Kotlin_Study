824904452406036ef486004503fa4edc52a7f8fd
Myodong <gyehd1230@naver.com>
2023-06-09 09:35:44 +0900

    Update MainActivity.kt

--- backButtonEx/app/src/main/java/com/myodong/backbuttonex/MainActivity.kt
+++ backButtonEx/app/src/main/java/com/myodong/backbuttonex/MainActivity.kt
@@ -2,6 +2,7 @@ package com.myodong.backbuttonex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 
@@ -14,12 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    override  fun onBackPressed(){
+    override fun onBackPressed() {
+
         // Log로 동작 하는지 확인해 보기
-        Log.d("MainActivity","backbutton 작동")
+        Log.d("MainActivity", "backbutton 작동")
 
         isDouble = true
-        Toast.makeText(this, "한번더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show()
-    }
+        // 메세지 출력
+        Toast.makeText(this, "한번더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
 
+        // 2초뒤 다시 변수 변경
+        Handler().postDelayed(Runnable {
+            isDouble=false
+        },2000)
+    }
 }
