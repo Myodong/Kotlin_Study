00c5cf0dda7de506b7b9295ba546fa3dcc57655a
Myodong <gyehd1230@naver.com>
2023-06-12 10:03:44 +0900

    .

---
+++ trot_list/app/src/main/java/com/myodong/trot_list/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.myodong.trot_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        // 1. navigation 만들기
+        
+        // 2. Fragemnt 만들고 연결하기 네비게이션에
+        
+        // 3. 리사이클러뷰 만들기
+    }
+}
