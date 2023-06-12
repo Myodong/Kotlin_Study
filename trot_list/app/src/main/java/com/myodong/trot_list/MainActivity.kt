728ddb2404578642064a855a68c2d2d616f11e70
Myodong <gyehd1230@naver.com>
2023-06-12 10:54:28 +0900

    .

--- trot_list/app/src/main/java/com/myodong/trot_list/MainActivity.kt
+++ trot_list/app/src/main/java/com/myodong/trot_list/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         // 1. navigation 만들기
         
         // 2. Fragemnt 만들고 연결하기 네비게이션에
-            //2-1 Fragment에서 버튼 누르면 이동하기
-        
+            //2-1 Fragment에서 버튼(이미지) 누르면 이동하기
+
         // 3. 리사이클러뷰 만들기
     }
 }
--- trot_list/app/src/main/java/com/myodong/trot_list/Singer1Fragment.kt
+++ trot_list/app/src/main/java/com/myodong/trot_list/Singer1Fragment.kt
@@ -5,28 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Singer1Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Singer1Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +21,32 @@ class Singer1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_singer1, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_singer1, container, false)
+
+        // 방법 1
+//        view.findViewById<ImageView>(R.id.image1).setOnClickListener{
+//
+//        }
+        // 방법2
+//        val image1 = view.findViewById<ImageView>(R.id.image1)
+//        image1.setOnClickListener{
+//
+//        }
+        // 이미지 2번 클릭 시
+        val image2 = view.findViewById<ImageView>(R.id.image2)
+        image2.setOnClickListener{
+            // 2번 이미지로 이동
+            it.findNavController().navigate(R.id.action_singer1Fragment_to_singer2Fragment)
+        }
+        // 이미지 3번 클릭 시
+        val image3 = view.findViewById<ImageView>(R.id.image3)
+        image3.setOnClickListener{
+            // 3번 이미지로 이동
+            it.findNavController().navigate(R.id.action_singer1Fragment_to_singer3Fragment)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Singer1Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Singer1Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
