8b45f519210513d4514921cb0a97c3dc7019b43a
Myodong <gyehd1230@naver.com>
2023-06-16 14:59:43 +0900

    Update SplashActivity.kt

--- diet_memo/app/src/main/java/com/myodong/diet_memo/SplashActivity.kt
+++ diet_memo/app/src/main/java/com/myodong/diet_memo/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -25,14 +26,32 @@ class SplashActivity : AppCompatActivity() {
 
         try {
             Log.e("SPLASH",auth.currentUser!!.uid)
+            Toast.makeText(this,"기존 비회원 로그인이 되어있습니다.",Toast.LENGTH_SHORT).show()
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
         } catch (e: Exception){
-            Log.e("SPLASH","회원가입 시켜줘야함")
+            Log.e("SPLASH","회원가입을 해주세요")
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this,"비회원 로그인 성공",Toast.LENGTH_SHORT).show()
 
+                        // 성공시 화면 넘어감
+                        Handler().postDelayed({
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }, 3000)
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this,"비회원 로그인 실패",Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000)
+
     }
 }
