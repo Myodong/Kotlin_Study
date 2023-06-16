f6eba277643174c855d096f9807bf2bf4dc4e05b
Myodong <gyehd1230@naver.com>
2023-06-16 14:19:25 +0900

    .

--- diet_memo/app/src/main/java/com/myodong/diet_memo/SplashActivity.kt
+++ diet_memo/app/src/main/java/com/myodong/diet_memo/SplashActivity.kt
@@ -4,14 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 @Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // 현재 인증정보 가져옴
+        auth = Firebase.auth
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
