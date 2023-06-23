43bc31584d84c096179861c2f7bc5e1a0e40079c
Myodong <gyehd1230@naver.com>
2023-06-23 13:40:39 +0900

    .

---
+++ mango_contents/app/src/main/java/com/myodong/mango_contents/BookmarkActivity.kt
@@ -0,0 +1,13 @@
+package com.myodong.mango_contents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookmarkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+    }
+}
