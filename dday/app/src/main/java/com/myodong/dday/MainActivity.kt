2cdcbea1a96ce2d4066fd80137b00ae7b620781e
Myodong <gyehd1230@naver.com>
2023-06-26 08:50:45 +0900

    .

--- dday/app/src/main/java/com/myodong/dday/MainActivity.kt
+++ dday/app/src/main/java/com/myodong/dday/MainActivity.kt
@@ -2,11 +2,17 @@ package com.myodong.dday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startBtn)
+        val endButton   = findViewById<Button>(R.id.endBtn)
+
+
     }
 }
