5a01b0272ea45ea2abfbff8c956a0b1a4cabcdb1
Myodong <gyehd1230@naver.com>
2023-06-26 08:56:40 +0900

    Update MainActivity.kt

--- dday/app/src/main/java/com/myodong/dday/MainActivity.kt
+++ dday/app/src/main/java/com/myodong/dday/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.myodong.dday
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,33 @@ class MainActivity : AppCompatActivity() {
 
         val startButton = findViewById<Button>(R.id.startBtn)
         val endButton   = findViewById<Button>(R.id.endBtn)
+
+        var startDate=""
+
+        // 시작 버튼 클릭 시
+        startButton.setOnClickListener {
+
+            // 다이얼로그 띄우기
+
+            val today = GregorianCalendar()
+            val year =  today.get(Calendar.YEAR)
+            val month =  today.get(Calendar.MONTH)
+            val day =  today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                // 년월일 받아오기
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+                    startDate = "${year}+${month+1}+${dayOfMonth}"
+                }
+
+            },year,month,day)
+
+        }
+        // 종료 버튼 클릭 시
+        endButton.setOnClickListener {
+
+        }
 
 
     }
