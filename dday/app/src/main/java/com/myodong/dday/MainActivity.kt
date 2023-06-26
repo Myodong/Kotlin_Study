4b8609be805e49c48e5c1415b3900fce816f8bb4
Myodong <gyehd1230@naver.com>
2023-06-26 09:00:51 +0900

    .

--- dday/app/src/main/java/com/myodong/dday/MainActivity.kt
+++ dday/app/src/main/java/com/myodong/dday/MainActivity.kt
@@ -18,12 +18,12 @@ class MainActivity : AppCompatActivity() {
         val endButton   = findViewById<Button>(R.id.endBtn)
 
         var startDate=""
+        var endDate=""
 
         // 시작 버튼 클릭 시
         startButton.setOnClickListener {
 
             // 다이얼로그 띄우기
-
             val today = GregorianCalendar()
             val year =  today.get(Calendar.YEAR)
             val month =  today.get(Calendar.MONTH)
@@ -33,15 +33,34 @@ class MainActivity : AppCompatActivity() {
                 // 년월일 받아오기
                 override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
 
+                    // 아래 둘다 같은 코드
+                    //startDate= year.toString() + (month+1).toString()+dayOfMonth.toString()
                     startDate = "${year}+${month+1}+${dayOfMonth}"
                 }
 
             },year,month,day)
-
+            dlg.show()
         }
         // 종료 버튼 클릭 시
         endButton.setOnClickListener {
 
+            // 다이얼로그 띄우기
+            val today = GregorianCalendar()
+            val year =  today.get(Calendar.YEAR)
+            val month =  today.get(Calendar.MONTH)
+            val day =  today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                // 년월일 받아오기
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+                    // 아래 둘다 같은 코드
+                    //endDate= year.toString() + (month+1).toString()+dayOfMonth.toString()
+                    endDate = "${year}+${month+1}+${dayOfMonth}"
+                }
+
+            },year,month,day)
+            dlg.show()
         }
 
 
